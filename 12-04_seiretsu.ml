d0a19db463c963aff6236998e6e8612758141f77
jyami <eiji@jyami.net>
2024-09-25 00:04:56 +0900

    12-04 重複をはじきながら整列するように修正

--- 12-04_seiretsu.ml
+++ 12-04_seiretsu.ml
@@ -3,30 +3,18 @@
 (* ekimei_t型リスト受け取ったらそれをひらがな順に整列し、さらに駅の重複を取り除いたekimei_t型リストを返す *)
 (* seiretsu : ekimei_t list -> ekimei_t list *)
 
-let rec insert_sort_ekimei lst tgt = match lst with
+let rec insert_ekimei lst tgt = match lst with
   [] -> [tgt]
   | first :: rest -> 
     match first with {kana=fkana} -> 
       match tgt with {kana=tkana} ->
-        if tkana > fkana then first :: (insert_sort_ekimei rest tgt)
+        if tkana > fkana then first :: (insert_ekimei rest tgt)
+        else if tkana = fkana then tgt::rest
         else tgt :: first :: rest
 
-let rec sort_ekimei lst = match lst with
+let rec seiretsu lst = match lst with
   [] -> []
-  | first :: rest -> insert_sort_ekimei (sort_ekimei rest) first
-
-let rec seiretsu2 lst = match lst with
-  [] -> []
-  | first :: rest1 -> match rest1 with
-    [] -> [first]
-    | second :: rest2 -> 
-      match first with {kana=fkana} -> 
-        match second with {kana=skana} ->
-          if fkana = skana then first :: (seiretsu2 rest2) (* secondを外した *)
-          else first :: (seiretsu2 rest1)
-
-let seiretsu lst = seiretsu2 (sort_ekimei lst)
-
+  | first :: rest -> insert_ekimei (seiretsu rest) first
 
 let ekimei_list = [ 
 {kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"}; 
@@ -37,13 +25,6 @@ let ekimei_list = [
 ]
 
 let test0 = seiretsu [] = []
-let test1 = sort_ekimei ekimei_list = [ 
-{kanji="池袋"; kana="いけぶくろ"; romaji="ikebukuro"; shozoku="有楽町線"}; 
-{kanji="池袋"; kana="いけぶくろ"; romaji="ikebukuro"; shozoku="丸ノ内線"}; 
-{kanji="明治神宮前"; kana="めいじじんぐうまえ"; romaji="meijijinguumae"; shozoku="千代田線"}; 
-{kanji="代々木上原"; kana="よよぎうえはら"; romaji="yoyogiuehara"; shozoku="千代田線"}; 
-{kanji="代々木公園"; kana="よよぎこうえん"; romaji="yoyogikouen"; shozoku="千代田線"}; 
-]
 let test11 = seiretsu [{kanji="池袋"; kana="いけぶくろ"; romaji="ikebukuro"; shozoku="有楽町線"}; ] = [{kanji="池袋"; kana="いけぶくろ"; romaji="ikebukuro"; shozoku="有楽町線"}; ]
 let test12 = seiretsu ekimei_list = [
 {kanji="池袋"; kana="いけぶくろ"; romaji="ikebukuro"; shozoku="有楽町線"}; 
